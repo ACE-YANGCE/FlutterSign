88678f025c3ebca405cf89534d691bc0eb5892b7
yangc <yangce@Founder.com.cn>
2018-08-20 23:07:40 +0800

    登录页面小优化

--- ace_flutter01/lib/main.dart
+++ ace_flutter01/lib/main.dart
@@ -7,93 +7,222 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: '轻签到',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: '极速登录'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _phoneState, _pwdState = false;
+  String _checkStr;
+  TextEditingController _phonecontroller = new TextEditingController();
+  TextEditingController _pwdcontroller = new TextEditingController();
+
+  void _checkPhone() {
+    if (_phonecontroller.text.isNotEmpty &&
+        _phonecontroller.text.trim().length == 11) {
+      _phoneState = true;
+    } else {
+      _phoneState = false;
+    }
+  }
+
+  void _checkPwd() {
+    if (_pwdcontroller.text.isNotEmpty &&
+        _pwdcontroller.text.trim().length >= 6 &&
+        _pwdcontroller.text.trim().length <= 10) {
+      _pwdState = true;
+    } else {
+      _pwdState = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: '轻签到',
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text('极速登录'),
         ),
-        body: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-
+        body: new ListView(
           children: <Widget>[
-            new Padding(
-                padding: new EdgeInsets.all(30.0),
-                child: new Image.asset(
-                  'images/ic_launcher.png',
-                  scale: 1.8,
-                )),
-            new Padding(
-              padding: new EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 15.0),
-              child: new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    new Padding(
-                      padding: new EdgeInsets.fromLTRB(0.0, 0.0, 5.0, 0.0),
-                      child: new Image.asset(
-                        'images/icon_username.png',
-                        width: 40.0,
-                        height: 40.0,
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    new Expanded(
-                        child: new TextField(
-                          decoration: new InputDecoration(
-                            hintText: '请输入用户名',
-                          ),
-                        ))
-                  ]),
-            ),
-            new Padding(
-              padding: new EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 40.0),
-              child: new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    new Padding(
-                      padding: new EdgeInsets.fromLTRB(0.0, 0.0, 5.0, 0.0),
-                      child: new Image.asset(
-                        'images/icon_password.png',
-                        width: 40.0,
-                        height: 40.0,
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    new Expanded(
-                        child: new TextField(
-                          decoration: new InputDecoration(
-                            hintText: '请输入密码',
-                          ),
-                          obscureText: true,
-                        ))
-                  ]),
-            ),
-            new Container(
-              width: 340.0,
-              child: new Card(
-                color: Colors.blue,
-                elevation: 16.0,
-                child: new FlatButton(
-                    child: new Padding(
-                      padding: new EdgeInsets.all(10.0),
-                      child: new Text(
-                        '极速登录',
-                        style: new TextStyle(color: Colors.white, fontSize: 16.0),
-                      ),
+            new Column(
+              // Column is also layout widget. It takes a list of children and
+              // arranges them vertically. By default, it sizes itself to fit its
+              // children horizontally, and tries to be as tall as its parent.
+              //
+              // Invoke "debug paint" (press "p" in the console where you ran
+              // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
+              // window in IntelliJ) to see the wireframe for each widget.
+              //
+              // Column has various properties to control how it sizes itself and
+              // how it positions its children. Here we use mainAxisAlignment to
+              // center the children vertically; the main axis here is the vertical
+              // axis because Columns are vertical (the cross axis would be
+              // horizontal).
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+
+              children: <Widget>[
+                new Padding(
+                    padding: new EdgeInsets.all(30.0),
+                    child: new Image.asset(
+                      'images/ic_launcher.png',
+                      scale: 1.2,
                     )),
-              ),
+                new Padding(
+                  padding: new EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 15.0),
+                  child: new Stack(
+                    alignment: new Alignment(1.0, 1.0),
+                    //statck
+                    children: <Widget>[
+                      new Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            new Padding(
+                              padding:
+                                  new EdgeInsets.fromLTRB(0.0, 0.0, 5.0, 0.0),
+                              child: new Image.asset(
+                                'images/icon_username.png',
+                                width: 40.0,
+                                height: 40.0,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            new Expanded(
+                              child: new TextField(
+                                controller: _phonecontroller,
+                                keyboardType: TextInputType.phone,
+                                decoration: new InputDecoration(
+                                  hintText: '请输入用户名',
+                                ),
+                              ),
+                            ),
+                          ]),
+                      new IconButton(
+                        icon: new Icon(Icons.clear, color: Colors.black45),
+                        onPressed: () {
+                          _phonecontroller.clear();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                new Padding(
+                  padding: new EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 40.0),
+                  child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        new Padding(
+                          padding: new EdgeInsets.fromLTRB(0.0, 0.0, 5.0, 0.0),
+                          child: new Image.asset(
+                            'images/icon_password.png',
+                            width: 40.0,
+                            height: 40.0,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        new Expanded(
+                          child: new TextField(
+                            controller: _pwdcontroller,
+                            decoration: new InputDecoration(
+                              hintText: '请输入密码',
+                              suffixIcon: new IconButton(
+                                icon: new Icon(Icons.clear,
+                                    color: Colors.black45),
+                                onPressed: () {
+                                  _pwdcontroller.clear();
+                                },
+                              ),
+                            ),
+                            obscureText: true,
+                          ),
+                        ),
+                      ]),
+                ),
+                new Container(
+                  width: 340.0,
+                  child: new Card(
+                    color: Colors.blue,
+                    elevation: 16.0,
+                    child: new FlatButton(
+                      child: new Padding(
+                        padding: new EdgeInsets.all(10.0),
+                        child: new Text(
+                          '极速登录',
+                          style: new TextStyle(
+                              color: Colors.white, fontSize: 16.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        _checkPhone();
+                        _checkPwd();
+                        if (_phoneState && _pwdState) {
+                          _checkStr = '页面跳转下期见咯！';
+                        } else {
+                          if (!_phoneState) {
+                            _checkStr = '请输入11位手机号！';
+                          } else if (!_pwdState) {
+                            _checkStr = '请输入6-10位密码！';
+                          }
+                        }
+                        print(_checkStr);
+                        showDialog<Null>(
+                          context: context,
+                          barrierDismissible: false,
+                          child: new AlertDialog(
+                            title: new Text(
+                              '温馨提示',
+                              style: new TextStyle(
+                                color: Colors.black54,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            content: new Text(_checkStr),
+                            actions: <Widget>[
+                              new FlatButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: new Text('确定')),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
